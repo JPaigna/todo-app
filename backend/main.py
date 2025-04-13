46f4564bb15b97c64de2899530f0218fd408fe4c
JPaigna <126860913+JPaigna@users.noreply.github.com>
2025-04-14 00:12:30 +0800

    hello cagayan mindanao rani

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://transcendent-eclair-b23a88.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
