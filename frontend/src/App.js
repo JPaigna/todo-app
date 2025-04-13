7d5cc5cd0010dedcbcbcf35703cbb2383f6df771
JPaigna <126860913+JPaigna@users.noreply.github.com>
2025-04-13 23:56:47 +0800

    hello world kalibutan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "https://todo-app-backend-fopd.onrender.com/";
+const API_URL = "https://todo-app-backend-fopd.onrender.com/todos";
 
 function App() {
   const [tasks, setTasks] = useState([]);
